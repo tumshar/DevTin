d8c3ed4f8fa0d9c5e9babba4b8d4e58942bfb4d2
tushar <tushar.acharrya@gmail.com>
2025-11-12 21:56:55 +0530

    exploring postman

--- src/app.js
+++ src/app.js
@@ -2,11 +2,22 @@ const express = require("express");
 
 const app= express();
 
-app.use((req,res)=>{
-res.send("i am responding from server")
+app.use("/about",(req,res)=>{
+    res.send("ABOUT PAGE");
 });
 
-app.listen(7777,()=>{ 
-console.log("server is listening rn");
+app.get("/user",(req,res)=>{
+    res.send("name:'Tushar'\n age:23");
+}); 
 
+app.use("/",(req,res)=>{
+    res.send("SERVER RUNNING");
+});
+
+
+ 
+
+
+app.listen(7777,()=>{ 
+    console.log("server is listening rn");
 });
